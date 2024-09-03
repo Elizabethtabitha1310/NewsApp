84db005e7cba43d7a690d12b0dafd74849f6d83c
elizabethtabitha1310 <tabyeliza@gmail.com>
2024-09-03 13:33:57 +0300

    Retrofit instance to get the data

--- app/src/main/java/com/example/newsapp/api/NewsAPI.kt
+++ app/src/main/java/com/example/newsapp/api/NewsAPI.kt
@@ -1,4 +1,33 @@
 package com.example.newsapp.api
 
+import retrofit2.http.Query
+import com.example.newsapp.models.NewsResponse
+import com.example.newsapp.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+
+
 interface NewsAPI {
+
+    @GET("v2/top-headlines")
+    suspend fun getHeadlines(
+       @Query("country")
+       countryCode: String = "Ke",
+       @Query("page")
+       pageNumber: Int = 1,
+       @Query("apiKey")
+       apiKey:String = API_KEY
+    ):Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey:String = API_KEY
+    ):Response<NewsResponse>
 }
+
+
