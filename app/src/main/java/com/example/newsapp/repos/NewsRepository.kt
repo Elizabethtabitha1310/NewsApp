0cdd3a1d37299281ef8d690a47726cd0ee17646f
elizabethtabitha1310 <tabyeliza@gmail.com>
2024-09-03 15:14:42 +0300

    News repository

---
+++ app/src/main/java/com/example/newsapp/repos/NewsRepository.kt
@@ -0,0 +1,19 @@
+package com.example.newsapp.repos
+
+import com.example.newsapp.api.RetrofitInstance
+import com.example.newsapp.database.ArticleDatabase
+import com.example.newsapp.models.Article
+
+class NewsRepository (val database: ArticleDatabase){
+    suspend fun getHeadlines(countryCode:String,pageNumber:Int) =
+        RetrofitInstance.api.getHeadlines(countryCode,pageNumber)
+
+    suspend fun searchNews(searchQuery:String,pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery,pageNumber)
+
+    suspend fun upsert(article: Article) = database.getArticleDao().upsert(article)
+
+    fun getAllArticles() = database.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticles(article: Article) = database.getArticleDao().deleteArticles(article)
+}
