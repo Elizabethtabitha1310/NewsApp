8cc57b91b9c581232146ac242c25dc639ccec3ac
elizabethtabitha1310 <tabyeliza@gmail.com>
2024-09-03 12:42:09 +0300

    CONSTANT UTILS

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "3611d91ba4994e448ce76b4f10ef176f"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val   QUERY_PAGE_SIZE = "20"
+    }
+}
